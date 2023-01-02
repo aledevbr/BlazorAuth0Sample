45a3e853caeb32963f3f81d78501759e5fa5b6a9
Alexandre Rodrigues da Silva <alexandre@alesil.com.br>
2023-01-02 18:00:53 -0300

    Adicionado configuração do Auth0 Client e o pacote Nuget client

--- BlazorAuth0Sample/Server/Controllers/WeatherForecastController.cs
+++ BlazorAuth0Sample/Server/Controllers/WeatherForecastController.cs
@@ -12,22 +12,27 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    private readonly ILogger<WeatherForecastController> _logger;
+    private static readonly List<WeatherForecast> forecasts = new();
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    static WeatherForecastController()
     {
-        _logger = logger;
-    }
-
-    [HttpGet]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        forecasts.AddRange(Enumerable.Range(1,5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        }));
     }
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get() 
+        => forecasts;
+
+    [HttpPost]
+    public WeatherForecast CreateForecast(WeatherForecast forecast)
+    {
+        forecasts.Add(forecast);
+        return forecast;
+    }
+
 }
