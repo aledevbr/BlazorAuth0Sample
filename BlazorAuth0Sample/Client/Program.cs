d47c31d72a9b000123ed9b3e833d2a10a8c6fd34
Alexandre Rodrigues da Silva <alexandre@alesil.com.br>
2023-01-02 18:03:03 -0300

    Carregamento das configurações no program.cs

--- BlazorAuth0Sample/Client/Program.cs
+++ BlazorAuth0Sample/Client/Program.cs
@@ -9,4 +9,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+});
+
 await builder.Build().RunAsync();
