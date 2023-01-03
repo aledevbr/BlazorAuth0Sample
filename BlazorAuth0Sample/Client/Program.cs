97a7d0c69192c720ec47ad1e8d3e06f53d5ef246
Alexandre Rodrigues da Silva <alexandre@alesil.com.br>
2023-01-02 22:58:28 -0300

    Chamda protegida para a API

--- BlazorAuth0Sample/Client/Program.cs
+++ BlazorAuth0Sample/Client/Program.cs
@@ -1,13 +1,18 @@
 using BlazorAuth0Sample.Client;
 
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("WeatherAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
+    .CreateClient("WeatherAPI"));
 
 builder.Services.AddOidcAuthentication(options =>
 {
